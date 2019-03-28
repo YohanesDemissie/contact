c47ac7e2ef8cc933e97a9a4ff4a518ffa65b60f7
Yohanes Demissie <Yohanesdemissie@gmail.com>
2019-03-27 22:12:34 -0700

    changed button

--- email.php
+++ email.php
@@ -89,15 +89,13 @@ Thank you for contacting us. We will be in touch with you very soon.
 }
 ?>
 
-<! DOCTYPE <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css">
-    <script src="main.js"></script>
 </head>
 <body>
     <form name="contactform" method="post" action="send_form_email.php">
@@ -144,7 +142,7 @@ Thank you for contacting us. We will be in touch with you very soon.
 </tr>
 <tr>
  <td colspan="2" style="text-align:center">
-  <input type="submit" value="Submit">   <a href="http://www.freecontactform.com/email_form.php">Email Form</a>
+  <input type="submit" value="Submit">Email Form</a>
  </td>
 </tr>
 </table>
